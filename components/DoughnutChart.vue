dd39a1a06fc7a405732b071f1a3d8774b470113d
tushar-me <tusharimran.dev@gmial.com>
2024-05-14 22:33:02 +0600

    Color Updated

--- components/DoughnutChart.vue
+++ components/DoughnutChart.vue
@@ -6,24 +6,25 @@
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js'
 import { Doughnut } from 'vue-chartjs'
 ChartJS.register(ArcElement, Tooltip, Legend)
+
 export default {
-    name: 'DoughnutChart',
-    components: { Doughnut },
-    data() {
-        return {
-            chartData: {
-                labels: ['Used - 50 GB', 'Free 5 GB'],
-                datasets: [
-                    {
-                        backgroundColor: ['#00C16A', '#f2f2f2'],
-                        data: [80, 20]
-                    }
-                ]
-            },
-            chartOptions: {
-                responsive: true
-            }
-        }
+  name: 'DoughnutChart',
+  components: {Doughnut},
+  data() {
+    return {
+      chartData: {
+        labels: ['Used - 50 GB', 'Free 5 GB'],
+        datasets: [
+          {
+            backgroundColor: ['#00C16A', '#f2f2f2'],
+            data: [80, 20]
+          }
+        ]
+      },
+      chartOptions: {
+        responsive: true
+      }
     }
+  },
 }
 </script>
